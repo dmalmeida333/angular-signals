5f67674267ae3edf99167ede447a1fada164afd0
Diego <dmalmeida@gmail.com>
2025-07-29 14:26:02 -0500

    create a number signal

--- apm-begin/src/app/products/product-selection/product-selection.ts
+++ apm-begin/src/app/products/product-selection/product-selection.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, computed, linkedSignal, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ProductData } from '../product-data';
+import { Product } from '../product';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-product-selection',
-  imports: [],
+  imports: [FormsModule, CurrencyPipe],
   templateUrl: './product-selection.html',
   styleUrl: './product-selection.css'
 })
 export class ProductSelection {
-  pageTitle = 'Product Selection';
-
+  pageTitle = 'Product Selection';  
+  products = signal(ProductData.products);
+  selectedProduct = signal<Product | undefined>(undefined);
+  quantity = linkedSignal({
+    source:this.selectedProduct,
+    computation: p => 1
+  });
+  total = computed(() => (this.selectedProduct()?.price ?? 0) * this.quantity());
+  color = computed(() => this.total() > 200 ? 'green' : 'blue');
+  onDecrease() {
+    this.quantity.update(q => q <= 0 ? 0 : q - 1);
+  }
+  onIncrease() {
+    this.quantity.update(q => q + 1);
+  }
 }
